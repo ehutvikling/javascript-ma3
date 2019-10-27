d3354cff31540ac3313312b2efadb32cba1c3d3f
ehutvikling <51324514+ehutvikling@users.noreply.github.com>
2019-10-27 19:55:42 +0100

    Update script.js

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,44 @@
 //Use RegEx to validate form
+
+function validateEmail(input) {
+
+    var emailTest = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    return emailTest.test(input);
+}
+
+function validatePhone(input) {
+
+    var phoneTest = /^\d+$/;
+    return phoneTest.test(input);
+}
+
+function validateForm(event) {
+    console.log("validateForm");
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var phonenumber = document.getElementById("phonenumber").value;
+    var email = document.getElementById("email").value;
+    if (firstname === "") {
+        alert("You have to write your firstname");
+        event.preventDefault();
+        return false;
+    }
+    if (lastname === "") {
+        alert("You have to write your lastname");
+        event.preventDefault();
+        return false;
+    }
+    if (!validatePhone(phonenumber)) {
+        alert("Phonenumber is not entered or is invalid");
+        event.preventDefault();
+        return false;
+    }
+    if (!validateEmail(email)) {
+        alert("Email is not entered or is invalid");
+        event.preventDefault();
+        return false;
+    }
+
+    alert("Form submitted successfully");
+}
